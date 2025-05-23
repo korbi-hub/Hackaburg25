5f581d3f774fcc10cb663ed5a2f45c3a83b6d6ac
korbi-hub <korbinian.kaiser@gmx.de>
2025-05-23 10:20:45 +0200

    Updated routing

---
+++ mobile_app/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,41 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get home => 'Home screen';
+
+  @override
+  String get registerBike => 'Register bike';
+
+  @override
+  String get registerYourBike => 'Register your bike';
+
+  @override
+  String get activateNcf => 'Activate your lock via RFID';
+
+  @override
+  String get activateNfcMessage =>
+      'To connect to your bike lock you first need to activate the Bike with the RFID chip you received with the lock.\n\nPlease place the RFID chip on the lock and wait until you can hear it beep. After you heard the noise, please press on \"Continue process\"';
+
+  @override
+  String get continueProcess => 'Continue process';
+
+  @override
+  String get createConnection => 'Connect to your lock';
+
+  @override
+  String get connectionSuccess => 'Connection successful';
+
+  @override
+  String get connectionFailed => 'Oops, we failed to connect to your lock';
+
+  @override
+  String get stats => 'Stats';
+}
---
+++ mobile_app/lib/screens/register_bike/cubit/bike_registration_state.dart
@@ -0,0 +1,16 @@
+part of 'bike_registration_cubit.dart';
+
+@immutable
+sealed class BikeRegistrationState {}
+
+final class RfidActivation extends BikeRegistrationState {}
+
+final class EstablishConnection extends BikeRegistrationState {}
+
+final class ConnectionSuccess extends BikeRegistrationState {}
+
+final class ConnectionError extends BikeRegistrationState {
+  final ErrorScreenConfig config;
+
+  ConnectionError(this.config);
+}
