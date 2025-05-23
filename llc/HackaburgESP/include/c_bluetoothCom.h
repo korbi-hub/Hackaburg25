5719f28e5284239b51c9e8341e2849155eeee1f9
Korbinian Graf <korbinian.graf@baumann-automation.com>
2025-05-24 00:09:14 +0200

    MLE eingefuegt

--- llc/HackaburgESP/include/c_bluetoothCom.h
+++ llc/HackaburgESP/include/c_bluetoothCom.h
@@ -2,6 +2,9 @@
 
 #include <string>
 #include <iostream>
+#include <Arduino.h>
+#include <c_BLE.h>
+
 
 class BluetoothCom{
 public:
@@ -9,6 +12,8 @@ BluetoothCom();
 std::string getComm(); // liefert String ohne extra Protokoll an Hauptklasse
 void sendComm(std::string);    // macht String Protokollreif und sendet dann an App
 bool isConnected(std::string* str_UUID); // gibt Status von connected aus und liefert ggf. gelesene UUID
+
 private:
     bool connected;
+    BLEHandler BLE;
 };
